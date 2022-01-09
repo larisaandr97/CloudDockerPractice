4e17c35e4806a38f82fc3e79d46c60f7a98fd9ef
larisaandr97 <androniclarisa97@yahoo.com>
2022-01-09 21:49:34 +0200

    Finished part two of tutorial with microservices communicating with each other.

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,18 +1,33 @@
 package com.amigoscode.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
     public void registerCustomer(final CustomerRegistrationRequest request) {
-        Customer customer = Customer.builder()
+        final Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
         //TODO: check if email valid
         //TODO: check if email not taken
-        //TODO: store customer in db
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        //TODO: check if fraudster
+        final FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId());
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+        //todo: send notification
     }
 }
