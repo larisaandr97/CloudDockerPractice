e51caff7be9592d5e5e6645a2326041312748afe
doitacademyjavacursants5 <doitacademyjavacursants5@gmail.com>
2022-01-10 22:20:41 +0200

    Product - microservice

---
+++ customer/src/main/java/com/amigoscode/customer/ProductCheckResponse.java
@@ -0,0 +1,4 @@
+package com.amigoscode.customer;
+
+public record ProductCheckResponse(Boolean isAnyProduct) {
+}
---
+++ product/src/main/java/com/amigoscode/product/ProductCheckResponse.java
@@ -0,0 +1,4 @@
+package com.amigoscode.product;
+
+public record ProductCheckResponse(Boolean isAnyProduct) {
+}
